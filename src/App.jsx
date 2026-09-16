02a207270b36d655da0eee57d2c36b018712d55a
edisonTri <fotossatick201@gmail.com>
2026-09-16 18:58:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout.jsx";
-import Home from "./pages/Home.jsx";
-import Actualidad from "./pages/Actualidad.jsx";
-import InstitucionalMinisterio from "./pages/InstitucionalMinisterio.jsx";
-import ScrollToTop from "./components/ScrollToTop.jsx";
-
 export default function App(){
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/actualidad" element={<Layout><Actualidad /></Layout>} />
-        <Route path="/institucional" element={<Layout><InstitucionalMinisterio /></Layout>} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{padding:'50px', fontFamily:'sans-serif'}}>
+      <h1>ACHEC - Sitio en construcción</h1>
+      <p>Si ves esto, el deploy funciona </p>
+      <p>Estamos arreglando los componentes</p>
+    </div>
   )
 }
